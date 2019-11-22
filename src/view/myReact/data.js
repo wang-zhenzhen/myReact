dbd00f2f93eb371d5384e10dc9ed5d206d06ef69
wangzhen <1332525292@qq.com>
2019-11-22 10:00:36 +0800

    all

---
+++ src/view/myReact/data.js
@@ -0,0 +1,9 @@
+let myReactData=[
+    {
+        title:"我的第一个react页面",
+        content:`
+        <button>点击</button>
+        <p>我学写的第一个react页面，虽然不华丽，但是我已经很努力了！</p>`
+    }
+]
+export default myReactData;
